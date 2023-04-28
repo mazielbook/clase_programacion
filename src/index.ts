258affaa7e3f6217283e504c215c00671d969d8e
franciscop905 <fperezflores905@gmail.com>
2023-04-28 17:17:44 -0400

    Variables de Typescript

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+let variable1: string = "Valeria Torres"
+const variable2 = number 2024
+console.log("variable1")
+
+let variable: variable2
+cont variable2: number = 2014
+
+let variable3: null | undefined
+let variable4: any = "hhdhdhd"
+
+console.log(variable1 + variable2)
+ let variable6: Array<number> = [
+    3,
+    5,
+    7,
+    0
+ ]
+
